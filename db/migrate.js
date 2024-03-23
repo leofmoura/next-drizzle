cfac437447e8da26974f70199c51307ebc0cf5cf
Leonardo Moura <leofmoura@gmail.com>
2024-03-23 17:51:45 -0400

    delete migrations

--- db/migrate.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var postgres_js_1 = require("drizzle-orm/postgres-js");
-var migrator_1 = require("drizzle-orm/postgres-js/migrator");
-var postgres_1 = require("postgres");
-var sql = (0, postgres_1.default)("...", { max: 1 });
-var db = (0, postgres_js_1.drizzle)(sql);
-await (0, migrator_1.migrate)(db, { migrationsFolder: "drizzle" });
-await sql.end();
