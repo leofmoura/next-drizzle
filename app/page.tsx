5047d43410358098f0a4772632e6ad41fcddb44d
Leonardo Moura <leofmoura@gmail.com>
2024-03-23 18:33:27 -0400

    Opt-out for static gen

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 import { getData } from "@/actions/todo.actions";
 import Todos from "@/components/todos";
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const data = await getData();
